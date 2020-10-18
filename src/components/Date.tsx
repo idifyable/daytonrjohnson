536415fb029d0f1e817c0df74f7b9cccbac61943
Dayton Johnson <daytonrjohnson@gmail.com>
2020-10-18 16:33:29 -0500

    Fixing Date component

---
+++ src/components/Date.tsx
@@ -0,0 +1,10 @@
+import { parseISO, format } from 'date-fns';
+
+interface Props {
+  dateString: string;
+}
+
+export const Date: React.FC<Props> = ({ dateString }) => {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+};
