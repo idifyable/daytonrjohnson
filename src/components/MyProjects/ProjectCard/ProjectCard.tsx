a2694d3067d8d1494d34485c2b005dc3ae66fcfe
Dayton Johnson <daytonrjohnson@gmail.com>
2020-10-18 18:21:27 -0500

    Fixing buttons

--- src/components/MyProjects/ProjectCard/ProjectCard.tsx
+++ src/components/MyProjects/ProjectCard/ProjectCard.tsx
@@ -23,21 +23,20 @@ export const ProjectCard: React.FC<Props> = ({ projectData }) => {
             <ul className="bulleted">{technologiesUsed}</ul>
           </div>
         )}
-        {projectData.demoUrl ||
-          (projectData.repoUrl && (
-            <div className="buttons">
-              {projectData.demoUrl && (
-                <a href={projectData.demoUrl} target="_blank" rel="noopener noreferrer">
-                  <button type="button">View Live</button>
-                </a>
-              )}
-              {projectData.repoUrl && (
-                <a href={projectData.repoUrl} target="_blank" rel="noopener noreferrer">
-                  <button type="button">View Repo</button>
-                </a>
-              )}
-            </div>
-          ))}
+        {(projectData.demoUrl || projectData.repoUrl) && (
+          <div className="buttons">
+            {projectData.demoUrl && (
+              <a href={projectData.demoUrl} target="_blank" rel="noopener noreferrer">
+                <button type="button">View Live</button>
+              </a>
+            )}
+            {projectData.repoUrl && (
+              <a href={projectData.repoUrl} target="_blank" rel="noopener noreferrer">
+                <button type="button">View Repo</button>
+              </a>
+            )}
+          </div>
+        )}
       </div>
     </li>
   );
