91f46ac4a35bc09d30c6fd6eea3fcb840f19f40c
Dayton Johnson <daytonrjohnson@gmail.com>
2020-10-18 18:04:11 -0500

    Disabling projects/[id] page for now

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,45 +1,45 @@
-import Head from 'next/head';
-import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-import { getAllProjectIds, getProjectData } from '@lib/projects';
-import Layout from '@components/Layout';
-import { Date } from '@components/Date';
+// import Head from 'next/head';
+// import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+// import { getAllProjectIds, getProjectData } from '@lib/projects';
+// import Layout from '@components/Layout';
+// import { Date } from '@components/Date';
 
-interface Props {
-  postData: any;
-}
+// interface Props {
+//   postData: any;
+// }
 
-export const Post: NextPage<Props> = ({ postData }: Props) => {
-  return (
-    <Layout>
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-      <article>
-        <h1>{postData.title}</h1>
-        <div>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
-    </Layout>
-  );
-};
+// export const Post: NextPage<Props> = ({ postData }: Props) => {
+//   return (
+//     <Layout>
+//       <Head>
+//         <title>{postData.title}</title>
+//       </Head>
+//       <article>
+//         <h1>{postData.title}</h1>
+//         <div>
+//           <Date dateString={postData.date} />
+//         </div>
+//         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+//       </article>
+//     </Layout>
+//   );
+// };
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = getAllProjectIds();
-  return {
-    paths,
-    fallback: false,
-  };
-};
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const paths = getAllProjectIds();
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const postData = await getProjectData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
-};
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   const postData = await getProjectData(params.id);
+//   return {
+//     props: {
+//       postData,
+//     },
+//   };
+// };
 
-export default Post;
+// export default Post;
