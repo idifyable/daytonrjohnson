3fc4ac12059947abbfd3a510b67ff274374c8f81
Dayton Johnson <daytonrjohnson@gmail.com>
2020-10-18 18:14:35 -0500

    Implementing more projectData

--- src/components/MyProjects/ProjectCard/ProjectCard.tsx
+++ src/components/MyProjects/ProjectCard/ProjectCard.tsx
@@ -9,7 +9,7 @@ export const ProjectCard: React.FC<Props> = ({ projectData }) => {
     return <li key={technology}>{technology}</li>;
   });
   return (
-    <li id="wikipedia-search-page">
+    <li id={projectData.id}>
       <div className="image-wrapper">
         {projectData.image && (
           <img src={projectData.image} alt={projectData.title ? projectData.title : ''} />
@@ -17,20 +17,27 @@ export const ProjectCard: React.FC<Props> = ({ projectData }) => {
       </div>
       <div className="wrapper">
         <h1>{projectData.title}</h1>
-        <h2>Technologies Used</h2>
-        <ul className="bulleted">{technologiesUsed}</ul>
-        <div className="buttons">
-          {projectData.demoUrl && (
-            <a href={projectData.demoUrl} target="_blank" rel="noopener noreferrer">
-              <button type="button">View Live</button>
-            </a>
-          )}
-          {projectData.repoUrl && (
-            <a href={projectData.repoUrl} target="_blank" rel="noopener noreferrer">
-              <button type="button">View Repo</button>
-            </a>
-          )}
-        </div>
+        {technologiesUsed && (
+          <div>
+            <h2>Technologies Used</h2>
+            <ul className="bulleted">{technologiesUsed}</ul>
+          </div>
+        )}
+        {projectData.demoUrl ||
+          (projectData.repoUrl && (
+            <div className="buttons">
+              {projectData.demoUrl && (
+                <a href={projectData.demoUrl} target="_blank" rel="noopener noreferrer">
+                  <button type="button">View Live</button>
+                </a>
+              )}
+              {projectData.repoUrl && (
+                <a href={projectData.repoUrl} target="_blank" rel="noopener noreferrer">
+                  <button type="button">View Repo</button>
+                </a>
+              )}
+            </div>
+          ))}
       </div>
     </li>
   );
