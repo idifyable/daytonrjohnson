8cb726ae71a7b9fa21b8569f89a894a701c5df52
Dayton Johnson <daytonrjohnson@gmail.com>
2020-10-19 20:44:39 -0500

    feature/19: Converting NeonBillboard to styled-components

--- src/components/NeonBillboard.tsx
+++ src/components/NeonBillboard.tsx
@@ -1,8 +1,29 @@
+import styled from 'styled-components';
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 5rem 0 7rem 0;
+  max-width: 100%;
+  color: white;
+  text-align: center;
+`;
+const PrimaryHeading = styled.h1`
+  font: normal normal 80px/1em 'Permanent Marker', cursive;
+  animation: neon 7s ease-in-out infinite alternate;
+`;
+const SecondaryHeading = styled.h1`
+  font: normal normal 48px/1em 'Permanent Marker', cursive;
+  animation: neon 7s ease-in-out infinite alternate-reverse;
+`;
+
 export const NeonBillboard: React.FC = () => {
   return (
-    <section className="neon-billboard">
-      <h1>Dayton Johnson</h1>
-      <h2>Web Developer</h2>
-    </section>
+    <Section>
+      <PrimaryHeading>Dayton Johnson</PrimaryHeading>
+      <SecondaryHeading>Web Developer</SecondaryHeading>
+    </Section>
   );
 };
