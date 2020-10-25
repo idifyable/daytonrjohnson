317e96172753837aeae3313345030788c9c3ae32
Dayton Johnson <daytonrjohnson@gmail.com>
2020-10-25 18:13:59 -0500

    19: Creating global components folder

---
+++ src/components/globals/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Div = styled.div``;
+export const Ul = styled.ul``;
+export const Li = styled.li``;
+export const H1 = styled.h1``;
+export const H2 = styled.h2``;
+export const H3 = styled.h3``;
+export const H4 = styled.h4``;
+export const H5 = styled.h5``;
+export const H6 = styled.h6``;
+export const P = styled.p``;
+export const Strong = styled.strong``;
