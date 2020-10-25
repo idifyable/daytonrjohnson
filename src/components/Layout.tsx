5ccc933fc3fcb739d88b6fa6353b0f7091dee2d3
Dayton Johnson <daytonrjohnson@gmail.com>
2020-10-25 17:17:37 -0500

    19: Removing og:image meta tag

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -21,12 +21,6 @@ const Layout: NextPage<Props> = ({ children, home, className }: Props) => {
           name="description"
           content="Hello, I'm a web developer based in Eau Claire, Wisconsin. I'm passionate in coding and creating useful web-based software. I love learning new technologies and am focused on becoming a master of the web platform."
         />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
