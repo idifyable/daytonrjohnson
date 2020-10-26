b1398a51826adcf00a571a4d866ee1a3cc99bcfb
Dayton Johnson <daytonrjohnson@gmail.com>
2020-10-25 19:23:39 -0500

    19: Fixing mobile font size

--- src/components/typography/PrimaryHeading/style.tsx
+++ src/components/typography/PrimaryHeading/style.tsx
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import { breakpoints } from '@constants/breakpoints';
 
 export const PrimaryHeading = styled.h1`
-  font: normal bold 42px/1em 'Lato', sans-serif;
+  font: normal bold 32px/1em 'Lato', sans-serif;
 
   @media ${breakpoints.desktop} {
-    font-size: 32px;
+    font-size: 42px;
   }
 `;
--- src/components/typography/SecondaryHeading/style.tsx
+++ src/components/typography/SecondaryHeading/style.tsx
@@ -2,9 +2,9 @@ import { breakpoints } from '@constants/breakpoints';
 import styled from 'styled-components';
 
 export const SecondaryHeading = styled.h2`
-  font: normal normal 32px/1em 'Lato', sans-serif;
+  font: normal normal 24px/1em 'Lato', sans-serif;
 
   @media ${breakpoints.desktop} {
-    font-size: 24px;
+    font-size: 32px;
   }
 `;
