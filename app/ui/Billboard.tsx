5473e48e622036bf86e4eaa818360b5f366309a1
idifyable <daytonrjohnson@gmail.com>
2024-02-18 08:32:36 -0600

    Tweaking Billboard styling

--- app/ui/Billboard.tsx
+++ app/ui/Billboard.tsx
@@ -1,8 +1,10 @@
 export default function Billboard() {
   return (
-    <div className="flex flex-col justify-center align-center w-screen h-96 text-center bg-blue-700">
-      <h1 className="text-6xl mb-4">Dayton Johnson</h1>
-      <h2 className="text-4xl">Web Developer</h2>
+    <div className="flex flex-col justify-center align-center w-screen h-96  bg-gradient-to-b from-blue-700 to-transparent">
+      <div className="container text-center m-auto">
+        <h1 className="text-6xl mb-4">Dayton Johnson</h1>
+        <h2 className="text-4xl">Web Developer</h2>
+      </div>
     </div>
   );
 }
